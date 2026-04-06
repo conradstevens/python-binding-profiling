3f6c1d61f23cd3a4281121f57193224fe619bf26
Conrad <cste8212@acfr.usyd.edu.au>
2026-04-06 03:51:48 -0400

    refactor: testing

--- tests/test_equivilence.py
+++ tests/test_equivilence.py
@@ -38,124 +38,98 @@ MODULES_TO_TEST: list[ModuleProtocol] = [
     pybind11_bindings,
     nano_bindings,
     numba_python,
-    cython_module
+    cython_module,
 ]
 
-
-class TestEquivalence:
-    def setup_method(self):
-        self._x_y_val_test_pairs: tuple[tuple[int, int], ...] = ((1, 2), (3, 4), (5, 10), (100, 1000))
-        self._n_test_values: tuple[int, ...] = (1, 2, 3, 4, 10, 100, 1000)
-        self._my_class_test_init_args: tuple[tuple[float, float, int], ...] = (
-            (1, 2, 1), (3, 4, 2), (5, 6, 3), (7, 8, 100)
-        )
-
-        ref_module = packaged_raw_python
-        ref_my_class: tuple[MyClass, ...] = self._get_classes_module(ref_module)
-
-        self._py_addition_results: tuple[float, ...] = tuple(
-            ref_module.addition(*p) for p in self._x_y_val_test_pairs
-        )
-        self._py_addition_three_times_results: tuple[float, ...] = tuple(
-            ref_module.addition_three_times(*p) for p in self._x_y_val_test_pairs
-        )
-        self._py_fibonacci_results: tuple[list[float], ...] = tuple(
-            ref_module.fibonacci(n) for n in self._n_test_values
-        )
-        self._py_fibonacci_numpy_results: tuple[NDArray[np.float64], ...] = tuple(
-            ref_module.fibonacci_numpy(n) for n in self._n_test_values
-        )
-
-        self._my_class_py_addition_results: tuple[tuple[float, ...], ...] = tuple(
-            tuple(c.class_addition(*p) for p in self._x_y_val_test_pairs) for c in ref_my_class
-        )
-        self._my_class_py_addition_three_times_results: tuple[tuple[float, ...], ...] = tuple(
-            tuple(c.class_addition_three_times(*p) for p in self._x_y_val_test_pairs) for c in ref_my_class
-        )
-        self._my_class_py_fibonacci_results: tuple[tuple[list[float], ...], ...] = tuple(
-            tuple(c.class_fibonacci() for _ in self._n_test_values) for c in ref_my_class
-        )
-        self._my_class_py_fibonacci_numpy_results: tuple[tuple[NDArray[np.float64], ...], ...] = tuple(
-            tuple(c.class_fibonacci_numpy() for _ in self._n_test_values) for c in ref_my_class
-        )
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_addition(self, m):
-        results: tuple[float, ...] = tuple(m.addition(*p) for p in self._x_y_val_test_pairs)
-        assert results == pytest.approx(self._py_addition_results, abs=EQUIVALENCE_TOLERANCE, rel=0)
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_addition_three_times(self, m):
-        results: tuple[float, ...] = tuple(m.addition_three_times(*p) for p in self._x_y_val_test_pairs)
-        assert results == pytest.approx(self._py_addition_three_times_results, abs=EQUIVALENCE_TOLERANCE, rel=0)
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_fibonacci(self, m):
-        results: tuple[list[float], ...] = tuple(list(m.fibonacci(n)) for n in self._n_test_values)
-
-        assert results == pytest.approx(self._py_fibonacci_results, abs=EQUIVALENCE_TOLERANCE, rel=0)
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_fibonacci_numpy(self, m):
-        results: tuple[NDArray[np.float64], ...] = tuple(m.fibonacci_numpy(n) for n in self._n_test_values)
-        assert all(
-            np.allclose(a, b, atol=EQUIVALENCE_TOLERANCE)
-            for a, b in zip(results, self._py_fibonacci_numpy_results)
-        )
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_class_addition(self, m):
-        classes: tuple[MyClass, ...] = self._get_classes_module(m)
-        results: tuple[tuple[float, ...], ...] = tuple(
-            tuple(c.class_addition(*p) for p in self._x_y_val_test_pairs) for c in classes
-        )
-        assert all(r == pytest.approx(e, abs=EQUIVALENCE_TOLERANCE, rel=0)
-                   for r, e in zip(results, self._my_class_py_addition_results))
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_class_addition_three_times(self, m):
-        classes: tuple[MyClass, ...] = self._get_classes_module(m)
-        results: tuple[tuple[float, ...], ...] = tuple(
-            tuple(c.class_addition_three_times(*p) for p in self._x_y_val_test_pairs) for c in classes
-        )
-        assert all(r == pytest.approx(e, abs=EQUIVALENCE_TOLERANCE, rel=0)
-                   for r, e in zip(results, self._my_class_py_addition_three_times_results))
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_class_fibonacci(self, m):
-        classes: tuple[MyClass, ...] = self._get_classes_module(m)
-        results: tuple[tuple[list[float], ...], ...] = tuple(
-            tuple(list(c.class_fibonacci()) for _ in self._n_test_values) for c in classes
-        )
-        assert all(
-            inner_r == pytest.approx(inner_e, abs=EQUIVALENCE_TOLERANCE, rel=0)
-            for r, e in zip(results, self._my_class_py_fibonacci_results)
-            for inner_r, inner_e in zip(r, e)
-        )
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_class_fibonacci_numpy(self, m):
-        classes: tuple[MyClass, ...] = self._get_classes_module(m)
-        results: tuple[tuple[NDArray[np.float64], ...], ...] = tuple(
-            tuple(c.class_fibonacci_numpy() for _ in self._n_test_values) for c in classes
-        )
-        assert all(
-            np.allclose(a, b, atol=EQUIVALENCE_TOLERANCE)
-            for a, b in zip(results, self._my_class_py_fibonacci_numpy_results)
-        )
-
-    @pytest.mark.parametrize("m", MODULES_TO_TEST)
-    def test_mutability_of_my_class_members(self, m):
-        my_class: MyClass = m.MyClass(*(self._my_class_test_init_args[0]))
-
-        fib_l_ref = my_class.class_fibonacci()
-        fib_l_ref[0] = TEST_VAL
-        assert fib_l_ref == my_class._fib_l
-
-        fib_arr_ref = my_class.class_fibonacci_numpy()
-        fib_arr_ref[0] = TEST_VAL
-        assert all(fib_arr_ref == my_class._fib_arr)
+X_Y_PAIRS = ((1, 2), (3, 4), (5, 10), (100, 1000))
+N_VALUES = (1, 2, 3, 4, 10, 100, 1000)
+CLASS_INIT_ARGS = ((1, 2, 1), (3, 4, 2), (5, 6, 3), (7, 8, 100))
 
 
-    def _get_classes_module(self, m: ModuleProtocol | types.ModuleType) -> tuple:
-        return tuple(m.MyClass(*args) for args in self._my_class_test_init_args)
+@pytest.fixture
+def ref():
+    return packaged_raw_python
+
+
+@pytest.fixture
+def ref_classes(ref):
+    return tuple(ref.MyClass(*args) for args in CLASS_INIT_ARGS)
+
+
+def _make_classes(m):
+    return tuple(m.MyClass(*args) for args in CLASS_INIT_ARGS)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_addition(m, ref):
+    expected = tuple(ref.addition(*p) for p in X_Y_PAIRS)
+    results = tuple(m.addition(*p) for p in X_Y_PAIRS)
+    assert results == pytest.approx(expected, abs=EQUIVALENCE_TOLERANCE, rel=0)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_addition_three_times(m, ref):
+    expected = tuple(ref.addition_three_times(*p) for p in X_Y_PAIRS)
+    results = tuple(m.addition_three_times(*p) for p in X_Y_PAIRS)
+    assert results == pytest.approx(expected, abs=EQUIVALENCE_TOLERANCE, rel=0)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_fibonacci(m, ref):
+    expected = tuple(ref.fibonacci(n) for n in N_VALUES)
+    results = tuple(list(m.fibonacci(n)) for n in N_VALUES)
+    assert results == pytest.approx(expected, abs=EQUIVALENCE_TOLERANCE, rel=0)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_fibonacci_numpy(m, ref):
+    expected = tuple(ref.fibonacci_numpy(n) for n in N_VALUES)
+    results = tuple(m.fibonacci_numpy(n) for n in N_VALUES)
+    assert all(np.allclose(a, b, atol=EQUIVALENCE_TOLERANCE) for a, b in zip(results, expected))
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_class_addition(m, ref_classes):
+    classes = _make_classes(m)
+    for c, ref_c in zip(classes, ref_classes):
+        results = tuple(c.class_addition(*p) for p in X_Y_PAIRS)
+        expected = tuple(ref_c.class_addition(*p) for p in X_Y_PAIRS)
+        assert results == pytest.approx(expected, abs=EQUIVALENCE_TOLERANCE, rel=0)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_class_addition_three_times(m, ref_classes):
+    classes = _make_classes(m)
+    for c, ref_c in zip(classes, ref_classes):
+        results = tuple(c.class_addition_three_times(*p) for p in X_Y_PAIRS)
+        expected = tuple(ref_c.class_addition_three_times(*p) for p in X_Y_PAIRS)
+        assert results == pytest.approx(expected, abs=EQUIVALENCE_TOLERANCE, rel=0)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_class_fibonacci(m, ref_classes):
+    classes = _make_classes(m)
+    for c, ref_c in zip(classes, ref_classes):
+        results = list(c.class_fibonacci())
+        expected = list(ref_c.class_fibonacci())
+        assert results == pytest.approx(expected, abs=EQUIVALENCE_TOLERANCE, rel=0)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_class_fibonacci_numpy(m, ref_classes):
+    classes = _make_classes(m)
+    for c, ref_c in zip(classes, ref_classes):
+        assert np.allclose(c.class_fibonacci_numpy(), ref_c.class_fibonacci_numpy(), atol=EQUIVALENCE_TOLERANCE)
+
+
+@pytest.mark.parametrize("m", MODULES_TO_TEST)
+def test_mutability_of_my_class_members(m):
+    my_class = m.MyClass(*CLASS_INIT_ARGS[0])
+
+    fib_l_ref = my_class.class_fibonacci()
+    fib_l_ref[0] = TEST_VAL
+    assert fib_l_ref == my_class._fib_l
+
+    fib_arr_ref = my_class.class_fibonacci_numpy()
+    fib_arr_ref[0] = TEST_VAL
+    assert all(fib_arr_ref == my_class._fib_arr)
